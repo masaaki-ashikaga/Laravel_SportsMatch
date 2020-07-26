c34e27ae2e4f14c45d665eff76472cd899d900a1
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-07-26 17:25:14 +0900

    Sport Table作成

---
+++ database/migrations/2020_07_25_131725_create_sports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sports', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sport', '100');
+            $table->string('imagepath', '200');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sports');
+    }
+}
