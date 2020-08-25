25ff6937a90e617bc856ac8ee83b65326705264f
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-08-25 22:59:38 +0900

    フェイクデータ修正（イベント日付）

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -12,17 +12,17 @@ $factory->define(Event::class, function (Faker $faker) {
         'prefecture' => $faker->prefecture,
         'venue' => $faker->realText(10),
         'address' => $faker->address,
-        'event_start_date' => $faker->date,
+        'event_start_date' => $faker->dateTimeBetween('-1 week', '+1 week'),
         'event_start_time' => $faker->time,
-        'event_end_date' => $faker->date,
+        'event_end_date' => $faker->dateTimeBetween('+1 week', '+2 week'),
         'event_end_time' => $faker->time,
         'capacity' => $faker->numberBetween(2, 100),
         'payment' => $faker->numberBetween(1, 10000),
-        'apply_end_date' => $faker->date,
+        'apply_end_date' => $faker->dateTimeBetween('-2week', '-1 week'),
         'apply_end_time' => $faker->time,
         'event_imagepath' => 'test_event.jpg',
         'public' => $faker->numberBetween(0, 1),
         'sport_id' => $faker->numberBetween(1, 8),
-        'team_id' => $faker->numberBetween(1, 50)
+        'team_id' => $faker->numberBetween(1, 20)
     ];
 });
