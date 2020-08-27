4e4aca595a329628125c23a516e4c894ffaab950
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-08-28 08:48:17 +0900

    イベント一覧画面に状態表示

--- resources/views/events/event_index.blade.php
+++ resources/views/events/event_index.blade.php
@@ -7,7 +7,16 @@
             <div class="d-flex flex-wrap justify-content-between">
                 @foreach($events as $key => $event)
                 <div class="mb-5 align-top" style="width: 350px;">
-                    <p class="pt-2 mb-1 font-weight-bold">開催:{{ $event->event_start_date }}</p>
+                    <div class="d-flex">
+                        <p class="pt-2 mb-1 mr-4 font-weight-bold">開催:{{ $event->event_start_date }}</p>
+                        @if(date('Y-m-d') > $event->event_start_date)
+                            <p class="pt-2 pr-2 mb-1 font-weight-bold">イベント終了</p>
+                        @elseif(date('Y-m-d') > $event->apply_end_date)
+                            <p class="pt-2 pr-2 mb-1 font-weight-bold">受付終了</p>
+                        @else
+                            <p class="pt-2 pr-2 mb-1 font-weight-bold">募集中</p>
+                        @endif
+                    </div>
                     <div style="height:70px;">
                         <p class="mb-0"><i class="fas fa-map-marker-alt mr-1"></i>{{ $event->address }}</p>
                         <p><i class="fas fa-running"></i>{{ $event_genre[$key] }}</p>
